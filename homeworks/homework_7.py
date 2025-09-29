3267c78c60e65a3bd7d4bc81c0be5daf4ad95839
xzhyldyz <xzhldz@gmail.com>
2025-09-29 09:51:50 +0600

    Added lesson_7 and homework_7

--- homeworks/homework_7.py
+++ homeworks/homework_7.py
@@ -2,23 +2,31 @@ import sqlite3
 
 def create_table():
     conn.execute("DROP TABLE IF EXISTS books")
+    conn.execute("DROP TABLE IF EXISTS genres")
     conn.execute("""
          CREATE TABLE IF NOT EXISTS books (
              id INTEGER PRIMARY KEY AUTOINCREMENT,
              name TEXT,
              author TEXT,
              publication_year INTEGER,
-             genre TEXT,
              number_of_pages INTEGER,
-             number_of_copies INTEGER
+             number_of_copies INTEGER,
+             genre_id INTEGER,
+             FOREIGN KEY(genre_id)
+             REFERENCES genres(id)
         )
     """)
-
+    conn.execute("""
+    CREATE TABLE genres (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT
+    )
+    """)
 
 def add_book(name,author,publication_year,genre,number_of_pages, number_of_copies):
     conn.execute(
-        "INSERT INTO books (name, author, publication_year, genre, number_of_pages, number_of_copies) VALUES (?,?,?,?,?,?)",
-        (name, author, publication_year, genre, number_of_pages, number_of_copies)
+        "INSERT INTO books (name, author, publication_year, number_of_pages, number_of_copies) VALUES (?,?,?,?,?)",
+        (name, author, publication_year, number_of_pages, number_of_copies)
     )
     conn.commit()
 
@@ -28,23 +36,38 @@ def delete_book(book_id):
         (book_id,))
     conn.commit()
 
+def add_genre(name):
+    conn.execute("INSERT INTO genres (name) VALUES (?)", (name,))
+    conn.commit()
+
+def get_all_books():
+    result = conn.execute("SELECT * FROM "
+                          "books JOIN genres ON books.genre_id = genres.id ORDER BY books.id")
+    return result.fetchall()
+
 if __name__ == '__main__':
     conn = sqlite3.connect("database.db")
 
     create_table()
 
-    add_book("Martin Iden","Jack London", 1909, "Roman",500, 1000000)
-    add_book("Вишневый сад", "А.П. Чехов", 1903, "Пьеса", 90, 900000)
-    add_book("Лисы в винограднике", "Л. Фейхтвангер", 1947, "Роман", 550, 800000)
-    add_book("Первый учитель", "Ч. Айтматов", 1962, "Повесть", 120, 700000)
-    add_book("Белый клык", "Джек Лондон", 1906, "Роман", 350, 600000)
-    add_book("Война и мир", "Л. Толстой", 1869, "Роман", 1500, 2000000)
-    add_book("Как завоевать друзей ...", "Д. Карнеги", 1936, "Психология", 300, 500000)
-    add_book("До встречи с тобой", "Д. Мойес", 2012, "Роман", 500, 400000)
-    add_book("Бегущий за ветром", "Х. Хоссейн", 2003, "Роман", 390, 300000)
-    add_book("Атомные привычки", "Д. Клир", 2018, "Саморазвитие", 320, 200000)
+    add_genre("Роман")
+    add_genre("Повесть")
+    add_genre("Психология")
+
+    add_book("Martin Iden","Jack London", 1909,1, 500, 1000000)
+    add_book("Вишневый сад", "А.П. Чехов", 1903, 2, 90, 900000)
+    add_book("Лисы в винограднике", "Л. Фейхтвангер", 1947, 1, 550, 800000)
+    add_book("Первый учитель", "Ч. Айтматов", 1962, 2, 120, 700000)
+    add_book("Белый клык", "Джек Лондон", 1906, 1, 350, 600000)
+    add_book("Война и мир", "Л. Толстой", 1869, 1, 1500, 2000000)
+    add_book("Как завоевать друзей ...", "Д. Карнеги", 1936, 3, 300, 500000)
+    add_book("До встречи с тобой", "Д. Мойес", 2012, 1, 500, 400000)
+    add_book("Бегущий за ветром", "Х. Хоссейн", 2003, 1, 390, 300000)
+    add_book("Атомные привычки", "Д. Клир", 2018, 3, 320, 200000)
 
     delete_book(2)
     delete_book(7)
+
+    print(get_all_books())
 
     conn.close()
--- lessons/lesson_7.py
+++ lessons/lesson_7.py
@@ -1,39 +1,71 @@
 import sqlite3
 
-def crate_table():
+
+
+def create_table():
     conn.execute("DROP TABLE IF EXISTS students")
+
+    conn.execute("""
+    CREATE TABLE IF NOT EXISTS department (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT
+    )
+    """)
     conn.execute("""
          CREATE TABLE IF NOT EXISTS students (
              id INTEGER PRIMARY KEY AUTOINCREMENT,
              name TEXT,
              age INTEGER,
-             city TEXT
+             city TEXT,
+             department_id INTEGER,
+             FOREIGN KEY(department_id)
+             REFERENCES department(id)
         )
     """)
 
-def add_student(name, age, city):
+def add_student(name, age, city, department_id):
     conn.execute(
-        "INSERT INTO students (name, age, city) VALUES (?, ?, ?)",
-        (name, age, city)
+        "INSERT INTO students (name: str, age: int, city: str, department_id: int) VALUES (?, ?, ?)",
+        (name, age, city, department_id)
     )
     conn.commit()
 
-def delete_student(student_id):
+def delete_student(student_id: int):
     conn.execute(
         "DELETE FROM students WHERE id = ?",
         (student_id,))
     conn.commit()
 
+def get_all_students():
+    result = conn.execute("SELECT name, age FROM students ORDER BY age LIMIT 2")
+    return result.fetchall()
+
+def get_student(name: str):
+    result = conn.execute("SELECT * FROM students WHERE name = ?", (name,))
+    return result.fetchall()
+
+def update_student(student_id: int, new_name: str):
+    conn.execute("UPDATE students SET name = ? WHERE id = ?", (new_name, student_id))
+
+def add_department(name: str):
+    conn.execute("INSERT INTO department (name) VALUES (?)", (name,))
+    conn.commit()
 
 if __name__ == '__main__':
     conn = sqlite3.connect("database.db")
 
-    crate_table()
+    create_table()
+
+    add_department("Backend")
+    add_department("Frontend")
     
-    add_student("Aibek", 23, "Bishkek")
-    add_student("Aisulu", 19, "Karakol")
+    add_student("Aibek", 23, "Bishkek", 1)
+    add_student("Aisulu", 19, "Karakol", 2)
+    add_student("Maksat", 22, "Bishkek", 1)
 
     delete_student(1)
+    update_student(4, "Meerim")
+
+    print(get_all_students())
 
     conn.close()
-
